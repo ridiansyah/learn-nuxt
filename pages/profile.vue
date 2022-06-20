b193d853e173b3671b8c9afd725b5afdede63d10
Wahyu Ridiansyah <wahyuridiansyah@gmail.com>
2022-06-20 11:08:23 +0700

    try to make new page and some style

---
+++ pages/profile.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h2>Hello Dude!</h2>
+        <p>Nuxt it's so cool..</p>
+    </div>
+</template>
+
+<style>
+
+html,
+body {
+  padding: 0;
+  margin: 0;
+}
+body {
+width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+}
+
+h2 {
+    font-size: 48px;
+    color: #389acd;
+}
+
+p {
+    font-size: 18px;
+    color: #ababab;
+}
+</style>
